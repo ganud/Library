edb0198aca274a95b8c3d4176e443e0d942b1304
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-06 18:31:31 -0400

    Skeleton

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
