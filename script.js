69ea0032767ab5b8b2244664a0b1a5067b3ba6d1
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-07 18:16:38 -0400

    Basic display for books

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const myLibrary = [];
+
+const book1 = new Book("Joe's Wonderland", "Borgs", 200, true)
+const book2 = new Book("Bob's Wonderland", "Borgs", 200, true)
+const book4 = new Book("Jim's Wonderland", "Borgs", 200, true)
+
+const mainContent = document.querySelector('.main-content')
+const myLibrary = [book1, book2, book4];
 
 function Book(title, author, pages, read) {
-
-
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
   // the constructor...
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function populateLibrary() {
+  mainContent.innerHTML = "";
+  myLibrary.forEach((book) => {
+    let div = document.createElement('div');
+    div.classList.add("book-item");
+    div.innerHTML = `Title: ${book.title}<br/>Author: ${book.author}<br/>Pages: ${book.pages}<br/>Read: ${book.read}`;
+    mainContent.appendChild(div);
+  });
 }
+
+
+// For each book in myLibrary do something
+
