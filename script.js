02d7c19cda76ff9090b98d305f4ed3d329fbff22
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-07 18:35:17 -0400

    Add modal using w3schools code

--- script.js
+++ script.js
@@ -2,16 +2,40 @@
 const book1 = new Book("Joe's Wonderland", "Borgs", 200, true)
 const book2 = new Book("Bob's Wonderland", "Borgs", 200, true)
 const book4 = new Book("Jim's Wonderland", "Borgs", 200, true)
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("bookAdder");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 const mainContent = document.querySelector('.main-content')
 const myLibrary = [book1, book2, book4];
 
+// Book Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-  // the constructor...
 }
 
 function populateLibrary() {
@@ -24,6 +48,5 @@ function populateLibrary() {
   });
 }
 
-
-// For each book in myLibrary do something
+populateLibrary();
 
